3efb9fa082927a8a264f62064ea22a86a5d21001
Sean Mooney <smooney914@gmail.com>
2020-01-25 14:12:56 -0600

    header rough draft

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,27 +13,39 @@ export default class Header extends React.Component {
               </div>
               <div className="push-right header-nav">
                 <div className="header-nav-item">
-                  <FontAwesomeIcon className="header-nav-icon" icon={faUserAlt} />
+                  <div className="header-nav-icon">
+                    <FontAwesomeIcon icon={faUserAlt} />
+                  </div>
                   <div className="header-nav-label">About</div>
                 </div>
                 <div className="header-nav-item">
-                  <FontAwesomeIcon className="header-nav-icon" icon={faCoffee} />
+                  <div className="header-nav-icon">
+                    <FontAwesomeIcon icon={faCoffee} />
+                  </div>
                   <div className="header-nav-label">Experience</div>
                 </div>
                 <div className="header-nav-item">
-                  <FontAwesomeIcon className="header-nav-icon" icon={faFile} />
+                  <div className="header-nav-icon">
+                    <FontAwesomeIcon icon={faFile} />
+                  </div>
                   <div className="header-nav-label">Resume</div>
                 </div>
                 <div className="header-nav-item">
-                  <FontAwesomeIcon className="header-nav-icon" icon={faPhone} />
+                  <div className="header-nav-icon">
+                    <FontAwesomeIcon icon={faPhone} />
+                  </div>
                   <div className="header-nav-label">Contact</div>
                 </div>
                 <div className="header-nav-item">
-                  <FontAwesomeIcon className="header-nav-icon" icon={faBlog} />
+                  <div className="header-nav-icon">
+                    <FontAwesomeIcon icon={faBlog} />
+                  </div>
                   <div className="header-nav-label">Blog</div>
                 </div>
                 <div className="header-nav-item">
-                  <FontAwesomeIcon className="header-nav-icon" icon={faCoffee} />
+                  <div className="header-nav-icon">
+                    <FontAwesomeIcon icon={faCoffee} />
+                  </div>
                   <div className="header-nav-label">GitHub</div>
                 </div>
               </div>

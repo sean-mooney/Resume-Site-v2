17edb1cec6543ad15b302270d4bf5ede49415245
Sean Mooney <smooney914@gmail.com>
2020-01-28 22:24:56 -0600

    api is now separate project, integrated app with api

--- src/pages/blog/blog.js
+++ src/pages/blog/blog.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import {Container, Row} from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 import './blog.scss';
-import listReactFiles from 'list-react-files';
+import axios from 'axios';
+
+const apiUrl = "https://sean-mooney-blog.herokuapp.com/blog";
 
 export default class Blog extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {posts: []};
+        this.state = { posts: null, loadPage: false };
         this.getPosts = this.getPosts.bind(this);
     }
 
@@ -15,10 +17,13 @@ export default class Blog extends React.Component {
     }
 
     getPosts() {
-        // var fs = require('fs');
-        // var files = fs.readdirSync('/blogposts/');
-        // console.log(files);
-        //Maybe have a manifest.json that has a list of all blog posts then use href to get the file and load the data
+        axios.get(apiUrl + "/posts").then(res => {
+            if (res.status === 200) {
+                this.setState({ posts: res.data, loadPage: true });
+            }
+        }).catch(err => {
+            console.log("error retrieving posts", err);
+        });
     }
 
     render() {
@@ -26,9 +31,32 @@ export default class Blog extends React.Component {
             <div className="page-container">
                 <div className="content-container">
                     <Container className="blog-container">
-                        <Row className="blog-post-line">
-                            Hello Blog!
-                        </Row>
+                        {!this.state.loadPage ||
+                            <div className="blog-posts-container">
+                                {this.state.posts && this.state.posts.length > 0 ?
+                                    this.state.posts.map(post => {
+                                        return (
+                                            <Row className="blog-post-line">
+                                                <div className="blog-post-card">
+                                                    <div className="blog-post-title">
+                                                        {post.title}
+                                                    </div>
+                                                    <div className="blog-post-card-bottom">
+                                                        <div className="blog-post-upload-date">
+                                                            {post.uploadDate}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </Row>
+                                        );
+                                    })
+                                    :
+                                    <div className="blog-no-data">
+                                        Sorry, there's nothing here :(
+                            </div>
+                                }
+                            </div>
+                        }
                     </Container>
                 </div>
             </div>
